6e79791ab9a1096b0fe871bded8a11838d12ea8f
seonwoojh <swjh9523@gmail.com>
2024-08-31 00:21:35 +0900

    [MOD] main.tf

--- AWS_CLOUD/main.tf
+++ AWS_CLOUD/main.tf
@@ -8,12 +8,12 @@ module "vpc" {
   name = "Hybrid_VPC"
   cidr = "Insert Configuration"
 
-  azs                        = ["ap-northeast-2a"] # 가용영역 설정
-  private_subnets            = ["Insert Configuration"] # 서브넷 설정
-  create_igw                 = false # IGW 비활성화
-  enable_vpn_gateway         = true  # AWS VGW 생성
-  manage_default_network_acl = false # ACL 비활성화
-  propagate_private_route_tables_vgw = true # 라우팅 전파 활성화
+  azs                                = ["ap-northeast-2a"]      # 가용영역 설정
+  private_subnets                    = ["Insert Configuration"] # 서브넷 설정
+  create_igw                         = false                    # IGW 비활성화
+  enable_vpn_gateway                 = true                     # AWS VGW 생성
+  manage_default_network_acl         = false                    # ACL 비활성화
+  propagate_private_route_tables_vgw = true                     # 라우팅 전파 활성화
 
   ### Security_group 설정
   default_security_group_name = "vpn_security_group"
@@ -22,17 +22,17 @@ module "vpc" {
   default_security_group_ingress = [
     {
       description = "vpn_security_group"
-      cidr_blocks   = "Insert Configuration"
+      cidr_blocks = "Insert Configuration"
       from_port   = "Insert Configuration"
       to_port     = "Insert Configuration"
-      protocol = "Insert Configuration"
+      protocol    = "Insert Configuration"
     }
   ]
   ## Outbound Setting
   default_security_group_egress = [
     {
-      cidr_blocks   = "0.0.0.0/0"
-      protocol = "-1" # semantically equivalent to all ports
+      cidr_blocks = "0.0.0.0/0"
+      protocol    = "-1" # semantically equivalent to all ports
     }
   ]
 }
@@ -91,7 +91,7 @@ resource "aws_vpn_connection_route" "vpn_route" {
 
 # # VPN 연결 구성을 Local file로 저장한다.
 resource "local_file" "vpn_config" {
-  content  = aws_vpn_connection.SiteToSiteVPN.customer_gateway_configuration
+  content = aws_vpn_connection.SiteToSiteVPN.customer_gateway_configuration
   ### 안에 세부 옵션들 확인 ex ikev1
   filename = "Insert Configuration"
 }
